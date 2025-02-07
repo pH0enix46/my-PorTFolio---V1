be7439c71d18467133671810902a2e2b6dd5990e
pH0enix46 <mdjoy.javascript@gmail.com>
2025-02-07 16:31:46 +0600

    ✅ hero part ui chnage

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,12 +19,12 @@ export default function Hero() {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white relative dark:bg-grid-white/[0.2] bg-grid-black/[0.3] flex items-center justify-center">
+      <div className="h-screen w-full dark:bg-black-100 bg-white absolute dark:bg-grid-white/[0.2] bg-grid-black/[0.3] flex items-center justify-center top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[88vw] md:max-w-2xl lg:max-w-[60vw]">
+        <div className="max-w-[88vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <HeroDescription />
         </div>
       </div>
--- components/content/HeroDescription.tsx
+++ components/content/HeroDescription.tsx
@@ -1,13 +1,19 @@
 // // //
 
 import React from "react";
+import { TextGenerateEffect } from "../ui/text-generate-effect";
 
 export default function HeroDescription() {
   return (
-    <div>
-      <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-        HI djhuhfuihui
+    <div className="flex flex-col items-center">
+      <h2 className="uppercase tracking-widest text-[10px] md:text-sm text-blue-200 max-w-80 font-bold">
+        pH0enix46 protfolio --v1
       </h2>
+
+      <TextGenerateEffect
+        className="text-center text-[5vw] md:text-[4vw] lg:text-[3vw]"
+        words="Crafting Scalable and High-Performance Web Applications with the MERN Stack"
+      />
     </div>
   );
 }
