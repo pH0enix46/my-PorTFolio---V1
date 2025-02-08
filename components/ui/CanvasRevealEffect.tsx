ad1e2ad909fab7c5a1fd4487a69f4459cc561f48
pH0enix46 <mdjoy.javascript@gmail.com>
2025-02-09 01:13:53 +0600

    fixed huge huge bugs

--- components/ui/CanvasRevealEffect.tsx
+++ components/ui/CanvasRevealEffect.tsx
@@ -8,8 +8,6 @@ import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import React, { useMemo, useRef } from "react";
 import * as THREE from "three";
 
-
-
 // https://ui.aceternity.com/components/canvas-reveal-effect
 
 // Canvas Reveal Effect function ---------- ✅ ✅ ✅
